e57a73e91325dcabbb403a7f551c1cc1cc3fc01d
sravanreddy001 <sravanreddy001@895cee8a-1686-72c9-6d16-0942764fa0dc>
2012-04-13 00:12:22 +0000

    Completed requirements, (DONE) Implemented Update, Delete fuctions Delete local dump possible.

--- SimpleDHT/gen/edu/buffalo/cse/cse486_586/simpledht/R.java
+++ SimpleDHT/gen/edu/buffalo/cse/cse486_586/simpledht/R.java
@@ -14,13 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int textView=0x7f050000;
+        public static final int button3=0x7f050000;
+        public static final int button4=0x7f050001;
+        public static final int button5=0x7f050002;
+        public static final int textView=0x7f050003;
+        public static final int textView2=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int main2=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int deleteDump=0x7f040006;
+        public static final int dump=0x7f040005;
+        public static final int fetchRecord=0x7f040003;
         public static final int hello=0x7f040000;
+        public static final int insertRecord=0x7f040002;
+        public static final int test=0x7f040004;
     }
 }
